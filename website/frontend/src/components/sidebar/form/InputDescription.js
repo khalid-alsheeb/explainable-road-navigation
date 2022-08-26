62582bff2a1a76a8e286ff846f4e3168241407ca
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2022-08-26 23:58:17 +0300

    some more description

--- website/frontend/src/components/sidebar/form/InputDescription.js
+++ website/frontend/src/components/sidebar/form/InputDescription.js
@@ -12,11 +12,11 @@ const InputDescription = () => {
         {
             inputType === 1 ?
                 <Typography variant="subtitle2" color="textSecondary" align="left" style={{color: 'white'}}>
-                    Create your path by pressing on the edges you want. They should become orange or yellow, afterwards.
+                    Create your path by pressing on the edges you want. They should become orange or yellow, afterwards. You can only add edges starting from the source node, to the goal node.
                 </Typography>
             : inputType === 2 ?
                 <Typography variant="subtitle2" color="textSecondary" align="left" style={{color: 'white'}}>
-                    Create your path by dragging waypoint marker to where you want it.
+                    Create your path by dragging the waypoint marker to where you want it. Then choose which algorithm you want.
                 </Typography>
             : <></>
         }
