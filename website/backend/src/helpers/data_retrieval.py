64abc3fe3ac901d364ae5315168634548c314d73
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2022-08-29 01:38:04 +0300

    Directed Graph

--- website/backend/src/helpers/data_retrieval.py
+++ website/backend/src/helpers/data_retrieval.py
@@ -1,6 +1,9 @@
 import requests
 import pandas as pd
 import osmnx as ox
+import networkx as nx
+import math
+from shapely.geometry import Point, LineString
 
 from graph_helpers import fixWrongDataE
 
@@ -86,6 +89,62 @@ def add_specific_traffic_data(edges, name):
     #edges.loc[edges['name'] == name, 'free_flow_speed'] = traffic.freeFlowSpeed[0]
     #edges.loc[edges['name'] == name, 'traffic_confidence'] = traffic.confidence[0]
     edges.loc[edges['name'] == name, 'isClosed'] = (traffic.roadClosure[0]).astype(int)
+    
+    
+
+
+def multiDiGraphToDiGraph(G):
+    nodes, edges = ox.graph_to_gdfs(G)
+    toChange = edges.query('key == 1').copy()
+    
+    for index, row in toChange.iterrows():
+        coords = list(row['geometry'].coords)
+        maxDistance = 0
+        for i in range(len(coords) -  1):
+            pair = coords[i]
+            nextPair = coords[i+1]
+            
+            if (i == 0):
+                distance = math.sqrt( (pair[0] - nextPair[0])** 2 + (pair[1] - nextPair[1])**2 )      
+                
+            maxDistance += math.sqrt( (pair[0] - nextPair[0])** 2 + (pair[1] - nextPair[1])**2 )  
+            
+        ratio = distance / maxDistance
+
+        newEdge1 = row.copy()
+        newEdge2 = row.copy()
+
+        newEdge1.geometry = LineString([Point(x[0], x[1]) for x in coords[:2]])
+        newEdge2.geometry = LineString([Point(x[0], x[1]) for x in coords[1:]])
+
+        newEdge1.length = newEdge1.length * ratio
+        newEdge2.length = newEdge2.length * (1 - ratio)
+
+        newNode = nodes.loc[index[0]].copy()
+        newPoint = coords[1]
+        
+        newNode.geometry = Point(newPoint)
+        newNode.y = newPoint[1]
+        newNode.x = newPoint[0]
+        
+        newNodeName = round(int(str(index[0]) + str(index[1])) / 1e5)
+        
+        print(newNodeName in  nodes.index)
+        
+        edges.loc[(index[0], newNodeName, 0)] = newEdge1
+        edges.loc[(newNodeName, index[1], 0)] = newEdge2
+        
+        nodes.loc[newNodeName] = newNode
+        
+        print((index[0], newNodeName, 0), (newNodeName, index[1], 0), newNodeName)
+        
+    edges.drop(edges.query('key == 1').index, inplace=True)
+
+    G_new = ox.graph_from_gdfs(nodes, edges)
+    
+    return G_new
+
+
     
     
     
