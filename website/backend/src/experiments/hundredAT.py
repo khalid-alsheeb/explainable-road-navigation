455d289e66e12d436f9d2d492a52037b136bad38
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2022-09-04 14:30:55 +0300

    AT 6%

--- website/backend/src/experiments/hundredAT.py
+++ website/backend/src/experiments/hundredAT.py
@@ -11,7 +11,7 @@ G = ox.load_graphml('./data/graph-BH-1km-7-7-22-0130.graphml')
 variablesToUse = ['noWay', 'isClosed', 'maxSpeed', 'speed']
 variablesToUseFix(variablesToUse)
 
-data = pd.read_csv('./data/hundred_anytime_vs_original.csv')
+data = pd.read_csv('./data/hundred_anytime_vs_original_old2.csv')
 data.nodes = data.nodes.apply(literal_eval)
 
 nodes_100 = []
